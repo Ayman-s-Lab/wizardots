61b3f64445a24898e33554f3b8deceea0efc53c6
Namya <70416356+AymanLar@users.noreply.github.com>
2022-12-20 22:18:27 +0100

    add software

---
+++ install.sh
@@ -0,0 +1,11 @@
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
+     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
+     sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick  \
+     man-db xwallpaper python-pywal xclip \
+     zip unzip unrar light \
+     git sxhkd zsh pipewire pipewire-pulse \
+     arc-gtk-theme pcmanfm gnome-screenshot-git \
+     cowsay picom btop ranger \
+     pamixer ncmpcpp \
+     bluez bluez-utils
+
---
+++ readme.md
@@ -0,0 +1,38 @@
+## fonts: 
+-   noto-fonts
+- noto-fonts-emoji
+- noto-fonts-cjk
+- ttf-jetbrains-mono
+- ttf-joypixels
+- ttf-font-awesome
+
+## software:
+- sxiv
+- mpv
+- zathura
+- zathura-pdf-mupdf
+- ffmpeg
+- imagemagick
+- man-db
+- xwallpaper
+- python-pywal
+- xclip
+- zip
+- unzip
+- unrar
+- light
+- git
+- sxhkd
+- zsh
+- pipewire
+- pipewire-pulse
+- arc-gtk-theme 
+- dunst
+- cowsay
+- pamixer
+- ncmpcpp
+
+- bluez
+- bluez-utils
+
+
